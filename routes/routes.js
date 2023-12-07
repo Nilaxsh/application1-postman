9428a46c90ae9de35ea0332523fd9816593df696
Nilaxsh <nilaxsakana@gmail.com>
2023-12-07 15:20:48 +0530

    fourth commit

---
+++ routes/routes.js
@@ -0,0 +1,47 @@
+
+
+const express = require('express');
+
+const router = express.Router()
+
+module.exports = router;
+const Model = require('../model/model');
+//Post Method
+// router.post('/post', (req, res) => {
+//     res.send('Post API')
+// })
+
+//Get all Method
+router.get('/getAll', (req, res) => {
+    res.send('Get All API')
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+router.post('/post', (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error) {
+        res.status(400).json({message: error.message})
+    }
+})
